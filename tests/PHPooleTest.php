359f40fbfb982b7497b9d19613e0cac31b090a19
Narno <arnaud@ligny.org>
2015-03-30 14:55:38 +0200

    Adding tests + fixtures

--- tests/PHPooleTest.php
+++ tests/PHPooleTest.php
@@ -9,11 +9,40 @@
 namespace PHPoole\PHPooleTest;
 
 use PHPoole\PHPoole;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 class PHPooleTest extends \PHPUnit_Framework_TestCase
 {
+    protected $sourceDir;
+    protected $destDir;
+
+    public function setUp()
+    {
+        $this->sourceDir = (__DIR__ . '/fixtures/website');
+        $this->destDir   = $this->sourceDir;
+    }
+
+    public function tearDown()
+    {
+        $fs = new Filesystem();
+        $fs->remove($this->destDir . '/_site');
+    }
+
     public function testCreate()
     {
         $this->assertInstanceOf('PHPoole\PHPoole', PHPoole::create());
+    }
+
+    public function testLocateContent()
+    {
+        $iterator = Finder::create()
+            ->files()
+            ->in($this->sourceDir . '/content')
+            ->name('*.md');
+        $this->assertInstanceOf('Symfony\Component\Finder\Finder', $iterator);
+        $this->assertCount(1, $iterator);
+        $this->assertContainsOnlyInstancesOf('Symfony\Component\Finder\SplFileInfo', $iterator);
     }
 }
---
+++ tests/fixtures/website/content/Section1/Page1.md
@@ -0,0 +1,7 @@
+<!--
+tite: Page 1
+-->
+Page 1
+======
+
+Content of page 1.
