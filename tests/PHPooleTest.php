33554f23d2cc30ba8e154c6f11ae422d23151450
Arnaud Ligny <arnaud@ligny.org>
2015-04-03 01:15:02 +0200

    Adding tests

--- tests/PHPooleTest.php
+++ tests/PHPooleTest.php
@@ -18,27 +18,26 @@ use Symfony\Component\Finder\SplFileInfo;
 
 class PHPooleTest extends \PHPUnit_Framework_TestCase
 {
-    protected $sourceDir;
-    protected $destDir;
+    //protected $sourceDir;
+    //protected $destDir;
+    protected $iterator;
+    protected $file;
 
-    /*
     public function setUp()
     {
-        $this->sourceDir = (__DIR__ . '/fixtures/website');
-        $this->destDir   = $this->sourceDir;
+        //$this->sourceDir = (__DIR__ . '/fixtures/website');
+        //$this->destDir   = $this->sourceDir;
+        $this->iterator = $this->createContentIterator();
+        $this->file     = $this->createFile();
     }
 
+    /*
     public function tearDown()
     {
         $fs = new Filesystem();
         $fs->remove($this->destDir . '/_site');
     }
     */
-
-    public function testCreate()
-    {
-        $this->assertInstanceOf('PHPoole\PHPoole', PHPoole::create());
-    }
 
     public function createContentIterator()
     {
@@ -48,76 +47,120 @@ class PHPooleTest extends \PHPUnit_Framework_TestCase
             ->name('*.md');
     }
 
+    public function createFile()
+    {
+        return new SplFileInfo(
+            __DIR__ . '/fixtures/content/Section1/Page1.md',
+            'Section1',
+            'Section1/Page1.md'
+        );
+    }
+
+    public function testCreate()
+    {
+        $this->assertInstanceOf('PHPoole\PHPoole', PHPoole::create());
+    }
+
+    public function testOptions()
+    {
+        $options = [
+            'options' => [
+                'option1' => 'Option 1',
+                'option2' => 'Option 2',
+            ]
+        ];
+        $phpoole = (new PHPoole())->setOptions($options);
+        $this->assertEquals($options, $phpoole->getOptions());
+    }
+
     public function testContentIterator()
     {
-        $iterator = $this->createContentIterator();
+        $iterator = $this->iterator;
         $this->assertInstanceOf('Symfony\Component\Finder\Finder', $iterator);
         $this->assertCount(1, $iterator);
         $this->assertContainsOnlyInstancesOf('Symfony\Component\Finder\SplFileInfo', $iterator);
     }
 
+
     public function testParsePage()
     {
-        foreach($this->createContentIterator() as $file) {
-            $parsed = (new Page($file))->parse();
-            $this->assertStringEqualsFile(__DIR__ . '/fixtures/content_parsed/Page1.md/frontmatter.yaml', $parsed->getFrontmatter());
-            $this->assertStringEqualsFile(__DIR__ . '/fixtures/content_parsed/Page1.md/body.md', $parsed->getBody());
-        }
+        $parsed = (new Page($this->file))->parse();
+        $this->assertStringEqualsFile(__DIR__ . '/fixtures/content_parsed/Page1.md/frontmatter.yaml', $parsed->getFrontmatter());
+        $this->assertStringEqualsFile(__DIR__ . '/fixtures/content_parsed/Page1.md/body.md', $parsed->getBody());
     }
 
     public function testAddPageToCollection()
     {
-        foreach($this->createContentIterator() as $file) {
-            $page = new Page($file);
-            $pageCollection = new PageCollection();
-            $addResult = $pageCollection->add($page);
-            $this->assertArrayHasKey('section1/page1', $addResult);
-        }
+        $page = new Page($this->file);
+        $pageCollection = new PageCollection();
+        $addResult = $pageCollection->add($page);
+        $this->assertArrayHasKey('section1/page1', $addResult);
     }
 
     public function testConvertYaml()
     {
-        foreach($this->createContentIterator() as $file) {
-            $page = new Page($file);
-            $page->parse();
-            $variables = (new Converter())
-                ->convertFrontmatter(
-                    $page->getFrontmatter(),
-                    'yaml'
-                );
-            $this->assertArrayHasKey('title', $variables);
-            $this->assertArrayHasKey('date', $variables);
-        }
+        $page = new Page($this->file);
+        $page->parse();
+        $variables = (new Converter())
+            ->convertFrontmatter(
+                $page->getFrontmatter(),
+                'yaml'
+            );
+        $this->assertArrayHasKey('title', $variables);
+        $this->assertArrayHasKey('date', $variables);
     }
 
     public function testConvertMarkdown()
     {
-        foreach($this->createContentIterator() as $file) {
-            $page = new Page($file);
-            $page->parse();
-            $html = (new Converter())
-                ->convertBody($page->getBody());
-            $this->assertSame('<p>Content of page 1.</p>', $html);
-        }
+        $page = new Page($this->file);
+        $page->parse();
+        $html = (new Converter())
+            ->convertBody($page->getBody());
+        $this->assertSame('<p>Content of page 1.</p>', $html);
     }
 
     public function testConvertPage()
     {
         $pageCollection = new PageCollection();
-        foreach($this->createContentIterator() as $file) {
-            $page = new Page($file);
-            $page->parse();
-            $pageCollection->add($page);
-            $page = PHPoole::create()->convertPage($page, 'yaml');
-            $pageCollection->replace($page->getId(), $page);
-        }
-        foreach($pageCollection as $page) {
-            $this->assertObjectHasAttribute('title', $page);
-            $this->assertObjectHasAttribute('html', $page);
-            $this->assertObjectHasAttribute('variables', $page);
-            $this->assertSame('Page 1', $page->getTitle());
-            $this->assertSame('<p>Content of page 1.</p>', $page->getContent());
-            //$this->assertEquals(1427839200, $page['date'], '', 5);
-        }
+
+        $page = new Page($this->file);
+        $page->parse();
+        $pageCollection->add($page);
+
+        $page = PHPoole::create()->convertPage($page, 'yaml');
+        $pageCollection->replace($page->getId(), $page);
+        unset($page);
+
+        $page = $pageCollection->get('section1/page1');
+        $this->assertObjectHasAttribute('title', $page);
+        $this->assertObjectHasAttribute('html', $page);
+        $this->assertObjectHasAttribute('variables', $page);
+        $this->assertSame('Page 1', $page->getTitle());
+        $this->assertSame('<p>Content of page 1.</p>', $page->getContent());
+        //$this->assertEquals(1427839200, $page['date'], '', 5);}
+    }
+
+    public function testAddPage()
+    {
+        $page = new Page();
+        $pageCollection = new PageCollection();
+
+        $page->setId('id-of-page')
+            ->setTitle('title-of-page');
+        $pageCollection->add($page);
+
+        $this->assertContains($page, $pageCollection);
+    }
+
+    public function testGetPage()
+    {
+        $page = new Page();
+        $pageCollection = new PageCollection();
+
+        $page->setId('id-of-page')
+            ->setTitle('title-of-page');
+        $pageCollection->add($page);
+        
+        $this->assertNotNull($pageCollection->get('id-of-page'));
     }
 }
