a01062b303e9efe24a7b2557ae3dc83b61590734
Arnaud Ligny <arnaud@ligny.org>
2015-04-07 00:45:41 +0200

    Dedicated building tests

---
+++ tests/PHPooleBuild.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Copyright (c) Arnaud Ligny <arnaud@ligny.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHPoole\PHPooleTest;
+
+use PHPoole\PHPoole;
+use Symfony\Component\Filesystem\Filesystem;
+
+class PHPooleBuildTest extends \PHPUnit_Framework_TestCase
+{
+    protected $wsSourceDir;
+    protected $wsDestinationDir;
+
+    public function setUp()
+    {
+        $this->wsSourceDir      = __DIR__ . '/fixtures/website';
+        $this->wsDestinationDir = $this->wsSourceDir;
+    }
+
+    public function tearDown()
+    {
+        $fs = new Filesystem();
+        $fs->remove($this->wsDestinationDir . '/_site');
+    }
+
+    public function testBuid()
+    {
+        PHPoole::create(
+            $this->wsSourceDir,
+            null,
+            [
+                'site' => [
+                    'title'   => "My website",
+                    'baseurl' => 'http://localhost:8000/',
+                ],
+            ]
+        )->build();
+    }
+}
