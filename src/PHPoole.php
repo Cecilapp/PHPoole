82f945972eb7c4dd2909263c11b4b7ff2364176d
Narno <arnaud@ligny.org>
2016-02-09 23:39:34 +0100

    Deleting source and destination params
    Using setters (with backward compatibility) instead.

--- src/PHPoole.php
+++ src/PHPoole.php
@@ -102,68 +102,60 @@ class PHPoole implements EventsCapableInterface
     protected $fs;
 
     /**
-     * Constructor.
+     * PHPoole constructor.
      *
-     * @param null  $sourceDir
-     * @param null  $destDir
      * @param array $options
-     *
-     * @throws \Exception
      */
-    public function __construct($sourceDir = null, $destDir = null, $options = [])
+    public function __construct($options = [])
     {
-        if ($sourceDir === null) {
-            $sourceDir = getcwd();
+        // backward compatibility
+        $args = func_get_args();
+        if (count($args) > 1) {
+            $this->setSource($args[0]);
+            $this->setDestination($args[1]);
+            $options = $args[2];
+        } else {
+            $this->setSource(null);
+            $this->setDestination(null);
         }
-        if (!is_dir($sourceDir)) {
-            throw new \Exception(sprintf("'%s' is not a valid source directory.", $sourceDir));
-        }
-        if ($destDir === null) {
-            $destDir = $sourceDir;
-        }
-        if (!is_dir($destDir)) {
-            throw new \Exception(sprintf("'%s' is not a valid destination directory.", $destDir));
-        }
-        $this->sourceDir = $sourceDir;
-        $this->destDir = $destDir;
 
         $options = array_replace_recursive([
             'site' => [
-                'title'       => 'PHPoole', // site title
-                'baseline'    => 'A PHPoole website', // site baseline
-                'baseurl'     => 'http://localhost:8000/', // php -S localhost:8000 -t _site/ >/dev/null
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', // site description
-                'taxonomies'  => [ // list of taxonomies
-                    'tags'       => 'tag',      // tag vocabulary
-                    'categories' => 'category', // category vocabulary
+                'title'       => 'PHPoole',
+                'baseline'    => 'A PHPoole website',
+                'baseurl'     => 'http://localhost:8000/',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'taxonomies'  => [
+                    'tags'       => 'tag',
+                    'categories' => 'category',
                 ],
-                'paginate' => [ // pagination options
-                    'max'  => 5,      // maximum numbers of listed pages
-                    'path' => 'page', // ie: section/page/2
+                'paginate' => [
+                    'max'  => 5,
+                    'path' => 'page',
                 ],
             ],
             'content' => [
-                'dir' => 'content', // content directory (from source)
-                'ext' => 'md',      // file extension (*.md)
+                'dir' => 'content',
+                'ext' => 'md',
             ],
             'frontmatter' => [
-                'format' => 'yaml', // yaml or ini
+                'format' => 'yaml',
             ],
             'body' => [
-                'format' => 'md', // body format, Markdown by default
+                'format' => 'md',
             ],
             'static' => [
-                'dir' => 'static', // static files directory
+                'dir' => 'static',
             ],
             'layouts' => [
-                'dir' => 'layouts', // layouts/templates files directory
+                'dir' => 'layouts',
             ],
             'output' => [
-                'dir'      => '_site',      // output directory
-                'filename' => 'index.html', // default filename of generated files
+                'dir'      => '_site',
+                'filename' => 'index.html',
             ],
             'themes' => [
-                'dir' => 'themes', // themes directory
+                'dir' => 'themes',
             ],
         ], $options);
         if (!empty($options)) {
@@ -183,6 +175,48 @@ class PHPoole implements EventsCapableInterface
         $class = new \ReflectionClass(get_called_class());
 
         return $class->newInstanceArgs(func_get_args());
+    }
+
+    /**
+     * @param null $sourceDir
+     *
+     * @return $this
+     *
+     * @throws \Exception
+     */
+    public function setSource($sourceDir = null)
+    {
+        if ($sourceDir === null) {
+            $sourceDir = getcwd();
+        }
+        if (!is_dir($sourceDir)) {
+            throw new \Exception(sprintf("'%s' is not a valid source directory.", $sourceDir));
+        }
+
+        $this->sourceDir = $sourceDir;
+
+        return $this;
+    }
+
+    /**
+     * @param null $destDir
+     *
+     * @return $this
+     *
+     * @throws \Exception
+     */
+    public function setDestination($destDir = null)
+    {
+        if ($destDir === null) {
+            $destDir = $this->sourceDir;
+        }
+        if (!is_dir($destDir)) {
+            throw new \Exception(sprintf("'%s' is not a valid destination directory.", $destDir));
+        }
+
+        $this->destDir = $destDir;
+
+        return $this;
     }
 
     /**
