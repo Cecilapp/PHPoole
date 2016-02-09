42a70caa94d42c55e8542885cda080567373d57e
Narno <arnaud@ligny.org>
2016-02-09 20:23:13 +0100

    Deletes "@" used to "handle" error

--- src/PHPoole.php
+++ src/PHPoole.php
@@ -945,7 +945,7 @@ class PHPoole implements EventsCapableInterface
         $version = self::VERSION;
 
         if (file_exists(__DIR__.'/../composer.json')) {
-            @$composer = json_decode(file_get_contents(__DIR__.'/../composer.json'), true);
+            $composer = json_decode(file_get_contents(__DIR__.'/../composer.json'), true);
             if (isset($composer['version'])) {
                 $version = $composer['version'];
             }
