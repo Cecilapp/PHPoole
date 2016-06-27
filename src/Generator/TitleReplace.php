962834334c25d0771d6e214330c066be125b07d8
Narno <arnaud@ligny.org>
2016-06-27 17:30:53 +0200

    Creates "test" generator.

---
+++ src/Generator/TitleReplace.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Copyright (c) Arnaud Ligny <arnaud@ligny.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHPoole\Generator;
+
+use PHPoole\Page\Collection as PageCollection;
+use PHPoole\Page\Page;
+
+/**
+ * Class TitleReplace.
+ */
+class TitleReplace implements GeneratorInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function generate(PageCollection $pageCollection, \Closure $messageCallback)
+    {
+        $generatedPages = new PageCollection();
+
+        $filteredPages = $pageCollection->filter(function (Page $page) {
+            return false !== $page->getTitle();
+        });
+
+        /* @var $page Page */
+        foreach ($filteredPages as $page) {
+            $alteredPage = clone $page;
+            $alteredPage->setTitle(strtoupper($page->getTitle()));
+            $generatedPages->add($alteredPage);
+        }
+
+        return $generatedPages;
+    }
+}
