408dc921b668743c46282113f99988df807db5d5
Arnaud Ligny <arnaud@ligny.org>
2016-07-03 19:41:17 +0200

    Escapes deploy if PHP version <> 5.6.

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ REPO="PHPoole/phpoole.github.io"
 TARGET_BRANCH="source"
 DOCS_DIR="docs"
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+if [ $TRAVIS_PHP_VERSION != "5.6" -o "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy."
     exit 0
 fi
