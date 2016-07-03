eddf909b1a6acdfd595a25b9f018b4e363026dbb
Arnaud Ligny <arnaud@ligny.org>
2016-07-03 19:12:57 +0200

    Travis: deploy docs after success. (#86)
    * Travis: deploy script after success.
    * Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+SOURCE_BRANCH="master"
+REPO="PHPoole/phpoole.github.io"
+TARGET_BRANCH="source"
+DOCS_DIR="docs"
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy."
+    exit 0
+fi
+
+echo "Starting to update gh-pages..."
+
+cp -R $DOCS_DIR $HOME/$DOCS_DIR
+cd $HOME
+git config --global user.name "Travis"
+git config --global user.email "contact@travis-ci.org"
+git clone --quiet --branch=$TARGET_BRANCH https://${GH_TOKEN}@github.com/${REPO}.git gh-pages > /dev/null
+cd gh-pages
+cp -Rf $HOME/$DOCS_DIR/* .
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+git push -fq origin $TARGET_BRANCH > /dev/null
+exit 0
