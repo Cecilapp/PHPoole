816b5906f566a05fdc11fd6ed9b329653c214564
Arnaud Ligny <arnaud@ligny.org>
2016-07-03 19:27:08 +0200

    Docs deploy! (#88)
    * Update .travis.yml
    
    * Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ cp -R $DOCS_DIR $HOME/$DOCS_DIR
 cd $HOME
 git config --global user.name "Travis"
 git config --global user.email "contact@travis-ci.org"
-git clone --quiet --branch=$TARGET_BRANCH https://${GH_TOKEN}@github.com/${REPO}.git gh-pages > /dev/null
+git clone --quiet --branch=$TARGET_BRANCH https://${GITHUB_TOKEN}@github.com/${REPO}.git gh-pages > /dev/null
 cd gh-pages
 cp -Rf $HOME/$DOCS_DIR/* .
 git add -f .
