ace6d8a114434951a689a8080bc81d7a02752d9b
Arnaud Ligny <arnaud@ligny.org>
2016-08-30 14:34:56 +0000

    Applied fixes from StyleCI

--- src/Exception/Exception.php
+++ src/Exception/Exception.php
@@ -5,6 +5,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace PHPoole\Exception;
 
 class Exception extends \Exception implements ExceptionInterface
