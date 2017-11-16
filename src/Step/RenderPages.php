14c4597ba8adbea9881b531a4dcc65267c507d5a
Arnaud Ligny <arnaud@ligny.org>
2017-11-16 01:06:42 +0100

    Add timestamp in Twig globals

--- src/Step/RenderPages.php
+++ src/Step/RenderPages.php
@@ -62,7 +62,8 @@ class RenderPages extends AbstractStep
             ['menus' => $this->phpoole->getMenus()],
             ['pages' => $this->phpoole->getPages()->filter(function (Page $page) {
                 return $page->getVariable('published');
-            })]
+            })],
+            ['timestamp' => time()]
         ));
         $this->phpoole->getRenderer()->addGlobal('phpoole', [
             'url'       => 'https://phpoole.org/#v'.$this->phpoole->getVersion(),
