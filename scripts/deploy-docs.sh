89e3fcb5bb65df8b518d4d197b494295acaa3b7f
Arnaud Ligny <arnaud@ligny.org>
2018-07-17 00:23:14 +0200

    Update and rename deploy-docs.sh to scripts/deploy-docs.sh (#278)

---
+++ scripts/deploy-docs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+# Deploy documentation files to website
+
+SOURCE_BRANCH="master"
+TARGET_REPO="PHPoole/phpoole.github.io"
+TARGET_BRANCH="source"
+SOURCE_DOCS_DIR="docs"
+TARGET_DOCS_DIR="content/documentation"
+
+echo "Starting to update documentation..."
+cp -R $SOURCE_DOCS_DIR $HOME/$SOURCE_DOCS_DIR
+
+# clone target repo
+cd $HOME
+git config --global user.name "Travis"
+git config --global user.email "contact@travis-ci.org"
+git clone --quiet --branch=$TARGET_BRANCH https://${GH_TOKEN}@github.com/${REPO}.git ${TARGET_REPO} > /dev/null
+
+cd $TARGET_REPO
+mkdir -p $TARGET_DOCS_DIR
+# copy documentation dir
+cp -Rf $HOME/$SOURCE_DOCS_DIR/* $TARGET_DOCS_DIR
+
+# commit and push
+if [[ -n $(git status -s) ]]; then
+  git add -Af .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER: update ${TARGET_DOCS_DIR}"
+  git push -fq origin $TARGET_BRANCH > /dev/null
+else
+  echo "Nothing to update"
+fi
+exit 0
