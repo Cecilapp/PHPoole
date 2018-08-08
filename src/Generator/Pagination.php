9a44a4bff50d456585c70a1ab91f0390fd37da36
Arnaud Ligny <arnaud@ligny.org>
2018-08-08 19:31:57 +0200

    Sanitize permalinks. (#308)
    Delete "/index" in permalinks (fix #163).

--- src/Generator/Pagination.php
+++ src/Generator/Pagination.php
@@ -45,17 +45,17 @@ class Pagination extends AbstractGenerator implements GeneratorInterface
                 for ($i = 0; $i < $paginateCount; $i++) {
                     $pagesInPagination = array_slice($pages, ($i * $paginateMax), $paginateMax);
                     $alteredPage = clone $page;
-                    // first page
+                    // first page, if homepage id = 'index'
                     if ($i == 0) {
                         $alteredPage
-                            ->setId(Page::urlize(sprintf('%s/index', $path)))
+                            ->setId(Page::urlize(sprintf('%s/%s', $path, $path == '' ? 'index' : '')))
                             ->setPathname(Page::urlize(sprintf('%s', $path)))
                             ->setVariable('aliases', [
                                 sprintf('%s/%s/%s', $path, $paginatePath, 1),
                             ]);
                     } else {
                         $alteredPage
-                            ->setId(Page::urlize(sprintf('%s/%s/%s/index', $path, $paginatePath, $i + 1)))
+                            ->setId(Page::urlize(sprintf('%s/%s/%s/', $path, $paginatePath, $i + 1)))
                             ->setPathname(Page::urlize(sprintf('%s/%s/%s', $path, $paginatePath, $i + 1)))
                             ->unVariable('menu');
                     }
